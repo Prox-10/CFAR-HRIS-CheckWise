7119c19ebd2ec58f732b70a8aca11f9ba091d775
RJ Kyle G Labrador <kyledev10282001@gmail.com>
2025-08-22 02:11:01 +0800

    test

--- scripts/push.js
+++ scripts/push.js
@@ -6,3 +6,5 @@ const msg = process.argv.slice(2).join(' ') || 'update: commit via bun script';
 await $`git add .`;
 await $`git commit -m "${msg}"`;
 await $`git push`;
+await $`git pull origin main`;
+await $`git checkout main`;
