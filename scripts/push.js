8f80fbc013a3bd938963c11b1c6f401d02c520ae
RJ Kyle G Labrador <kyledev10282001@gmail.com>
2025-09-06 19:21:43 +0800

    test

--- scripts/push.js
+++ scripts/push.js
@@ -8,3 +8,7 @@ await $`git commit -m "${msg}"`;
 await $`git push`;
 await $`git pull origin main`;
 await $`git checkout main`;
+await $`git merge kyle`;
+await $`git push -u origin main`;
+await $`git checkout kyle`;
+await $`git pull origin main`;
